d0195c86aa0df25fab9551bfda5c72cbab3382a9
shayneofficer <shayneofficer@gmail.com>
2018-11-25 17:51:22 -0600

    lots of styling stuff mostly

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDf0O6SBe0AUBPqmDSy06OmQ6vIs7EVii4",
+    authDomain: "rock-paper-scissors-f71bc.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-f71bc.firebaseio.com",
+    projectId: "rock-paper-scissors-f71bc",
+    storageBucket: "rock-paper-scissors-f71bc.appspot.com",
+    messagingSenderId: "880680586895"
+};
+firebase.initializeApp(config);
